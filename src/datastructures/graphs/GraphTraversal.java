e790678fef4d6642bcc6d0470c1b21218a50d423
Atul Prakash Singh <atul.singh@altran.com>
2021-07-23 00:27:45 +0530

    Added Graph BFS & DFS Implementation.

--- src/datastructures/graphs/GraphTraversal.java
+++ src/datastructures/graphs/GraphTraversal.java
@@ -4,36 +4,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class GraphTraversal {
-
-    public static void main(String[] args) {
-        Vertex<Integer> vertex40 = new Vertex<>(40);
-        Vertex<Integer> vertex10 = new Vertex<>(10);
-        Vertex<Integer> vertex20 = new Vertex<>(20);
-        Vertex<Integer> vertex30 = new Vertex<>(30);
-        Vertex<Integer> vertex60 = new Vertex<>(60);
-        Vertex<Integer> vertex50 = new Vertex<>(50);
-        Vertex<Integer> vertex70 = new Vertex<>(70);
-
-        Graph<Integer> graph = new Graph(7);
-
-        graph.addEdge(vertex40, vertex10);
-        graph.addEdge(vertex40, vertex20);
-        graph.addEdge(vertex10, vertex30);
-        graph.addEdge(vertex20, vertex10);
-        graph.addEdge(vertex20, vertex30);
-        graph.addEdge(vertex20, vertex60);
-        graph.addEdge(vertex20, vertex50);
-        graph.addEdge(vertex30, vertex60);
-        graph.addEdge(vertex60, vertex70);
-        graph.addEdge(vertex50, vertex70);
-
-        graph.breadthFirstTraversal(vertex40);
-        System.out.println();
-        graph.depthFirstTraversal(vertex40);
-    }
-}
-
 class Graph<T> {
 
     private final int numberOfVertices;
@@ -105,5 +75,37 @@ class Vertex<T> {
 
     public T getData() {
         return data;
+    }
+}
+
+public class GraphTraversal {
+
+    public static void main(String[] args) {
+        Vertex<Integer> vertex40 = new Vertex<>(40);
+        Vertex<Integer> vertex10 = new Vertex<>(10);
+        Vertex<Integer> vertex20 = new Vertex<>(20);
+        Vertex<Integer> vertex30 = new Vertex<>(30);
+        Vertex<Integer> vertex60 = new Vertex<>(60);
+        Vertex<Integer> vertex50 = new Vertex<>(50);
+        Vertex<Integer> vertex70 = new Vertex<>(70);
+
+        Graph<Integer> graph = new Graph(7);
+
+        graph.addEdge(vertex40, vertex10);
+        graph.addEdge(vertex40, vertex20);
+        graph.addEdge(vertex10, vertex30);
+        graph.addEdge(vertex20, vertex10);
+        graph.addEdge(vertex20, vertex30);
+        graph.addEdge(vertex20, vertex60);
+        graph.addEdge(vertex20, vertex50);
+        graph.addEdge(vertex30, vertex60);
+        graph.addEdge(vertex60, vertex70);
+        graph.addEdge(vertex50, vertex70);
+
+        System.out.print("BFS -> ");
+        graph.breadthFirstTraversal(vertex40);
+        System.out.println();
+        System.out.print("DFS -> ");
+        graph.depthFirstTraversal(vertex40);
     }
 }
